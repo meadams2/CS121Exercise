c00b2eee1da5c8471440d1b0111601d08d6091d5
Marianne Adams <meadams2@bsu.edu>
2025-01-17 15:49:46 +0000

    Baby's first C

--- numberGuesser.c
+++ numberGuesser.c
@@ -1,2 +1,17 @@
 // Standard Number Guesser Game
 # include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <time.h>
+
+# define TRUE -1;
+# define FALSE 0;
+
+int main(){
+	char name[20];
+	printf("What is your name? ");
+	scanf("%s", name);
+	printf("Hi, %s! Let's play a game! \n", name);
+	int correct
+	return(0);
+}
