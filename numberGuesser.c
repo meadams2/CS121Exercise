8891ce45db6e246325e9296023b95b1057917826
Marianne Adams <meadams2@bsu.edu>
2025-01-21 17:13:48 +0000

    Yessss

--- numberGuesser.c
+++ numberGuesser.c
@@ -3,15 +3,47 @@
 # include <string.h>
 # include <stdlib.h>
 # include <time.h>
-
-# define TRUE -1;
-# define FALSE 0;
+# include <stdbool.h>
 
 int main(){
 	char name[20];
+	int turns = 0;
+	int correct;
+	int guess;
+	bool keepGoing;
+
+	srand(time(NULL));
+
 	printf("What is your name? ");
 	scanf("%s", name);
 	printf("Hi, %s! Let's play a game! \n", name);
-	int correct
-	return(0);
-}
+
+	correct =(rand() % 100) + 1;
+	printf("Correct: %d \n", correct);
+	
+	while(keepGoing){
+		turns++;
+		printf("Turns: %d \n", turns);
+		printf("Guess\n");
+		scanf("%d",&guess);
+		if (correct > guess){
+			printf("Too low \n");
+		}
+		else if (correct < guess){
+			printf("Too high \n");
+		}
+		else {
+			printf("You're so smart! \n");
+			keepGoing = false;
+		}		
+		if (turns < 7){
+			printf("Great \n");
+		}
+		if (turns > 7){
+			printf("You lose \n");
+			keepGoing = false;
+		}
+	} //end while loop	
+return(0);
+} //end main
+
